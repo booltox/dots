400a069edc9d55f7cbf7fb2b92b7c79057ecf795
booltox <booltox@gmail.com>
2022-12-06 21:45:33 -0600

    added neo-tree

--- init.lua
+++ init.lua
@@ -62,6 +62,19 @@ require('packer').startup(function(use)
   use 'Mofiqul/dracula.nvim'
   use 'arcticicestudio/nord-vim'
 
+  -- Unless you are still migrating, remove the deprecated commands from v1.x
+  vim.cmd([[ let g:neo_tree_remove_legacy_commands = 1 ]])
+
+  use {
+    "nvim-neo-tree/neo-tree.nvim",
+    branch = "v2.x",
+    requires = { 
+      "nvim-lua/plenary.nvim",
+      "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+      "MunifTanjim/nui.nvim",
+    }
+  }
+
   if is_bootstrap then
     require('packer').sync()
   end
